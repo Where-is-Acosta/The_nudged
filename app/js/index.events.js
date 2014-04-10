e31539147e8ad666742c8ee7b1c84e913d640264
Driptap <sg.sinan@gmail.com>
2014-04-10 11:08:09 +0200

    Event listener server

--- app/js/index.events.js
+++ app/js/index.events.js
@@ -1,16 +1,19 @@
 var fs = require('fs');
 var http = require('http');
+var url = require('url') ;
 
-http.createServer(function(request, responce){
+http.createServer(function (request, responce) {
   responce.writeHead(200);
   responce.write("Hello, this is dog");
   responce.end();
-  console.log(request + responce);
-  nudge(responce);
+
+  var queryObject = url.parse(request.url,true).query;
+  console.log(queryObject);
+  nudge(queryObject);
 }).listen(5454);
 
-function nudge(responce) {
-  var fragment = notifcation('<div><h1>Here is one</h1></div><p>' + responce + '</p>');
+function nudge(queryObject) {
+  var fragment = notifcation('<div><h1>Here is one</h1></div><p>' + queryObject + '</p>');
   document.body.insertBefore(fragment, document.body.childNodes[0]);
   document.getElementById("mix").style.display="block";
 }
